68978171201d984880e3430f2694d41fb49b2677
NoelVar <varnoel11@gmail.com>
2026-02-24 16:54:48 +0000

    Finished Homework2

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -3,7 +3,7 @@ const TMBD_IMAGES_ASSET_URL = "https://image.tmdb.org/t/p/w500/";
 
 const MovieCard = ({ movie }) => {
     return (
-        <Card className="group relative overflow-hidden cursor-pointer transition-all duration-200 hover:scale-105 hover:shadow-xl outline-blue-200 p-0 border-0 w-[7rem] h-[9.8rem] rounded-sm">
+        <Card className="movie-image group relative overflow-hidden cursor-pointer transition-all duration-200 hover:scale-105 hover:shadow-xl outline-blue-200 p-0 border-0 w-[7rem] h-[9.8rem] lg:w-[11.9rem] lg:h-[16.6rem] rounded-sm">
             <img 
                 src={movie?.poster_path ? TMBD_IMAGES_ASSET_URL + movie?.poster_path : "/placeholder.svg"}
                 alt={movie?.title}
